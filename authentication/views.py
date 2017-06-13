06df1f70548a3451c8b502db239a9c1327246285
NeuObito <xj174850@163.com>
2017-06-13 19:29:51 +0800

    update views.py

--- authentication/views.py
+++ authentication/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth import login, logout
 from django.views.generic import View
 from django.urls.resolvers import get_resolver
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse, HttpResponse
 
 from .models import User
 from .forms import LoginForm, RegisterForm
@@ -22,7 +22,15 @@ class RegisterView(View):
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.register()
-        return HttpResponse("Hello World!")
+        return JsonResponse({"msg": "Hello World."})
+
+    def options(self, request):
+        response = HttpResponse()
+        response['Access-Control-Allow-Headers'] = "accept, content-type"
+        response['Access-Control-Allow-Method'] = "POST"
+        response['Access-Control-Allow-Origin'] = "*"
+
+        return HttpResponse(response, content_type="application/json")
 
 
 class LoginView(View):
@@ -34,4 +42,4 @@ class LoginView(View):
         print("===========")
         if form.is_valid():
             form.login()
-        return HttpResponse("Hello World!")
+        return JsonResponse("Hello World!")
